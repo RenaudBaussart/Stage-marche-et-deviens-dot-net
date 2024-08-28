2f615af32e18377ee57fd18915c45a2b65990a13
Lucas Kucheida <kucheida.lucas@gmail.com>
2024-08-28 11:32:44 +0200

    feat(Repositories): Creation de l interface et du repository Session

---
+++ stage-marche-devient/stage-marche-devient/Repositorys/ISessionRepository.cs
@@ -0,0 +1,13 @@
+﻿using stage_marche_devient.Models;
+
+namespace stage_marche_devient.Repositories
+{
+    public interface ISessionRepository
+    {
+        IAsyncEnumerable<Session> GetAllAsync();
+        Task<Session?> GetByIdAsync(Guid id);
+        Task CreateAsync(Session session);
+        Task UpdateAsync(Session session);
+        Task DeleteAsync(Guid id);
+    }
+}
---
+++ stage-marche-devient/stage-marche-devient/Repositorys/SessionRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using stage_marche_devient.Models;
+using stage_marche_devient.Data;
+using stage_marche_devient.Repositories;
+
+namespace stage_marche_devient.Repositorys
+{
+    public class SessionRepository : ISessionRepository
+    {
+        private readonly ApiDbContext _context;
+        public SessionRepository(ApiDbContext context)
+            => _context = context;
+
+        public async IAsyncEnumerable<Session> GetAllAsync()
+        {
+            await foreach (var session in _context.Sessions)
+            {
+                yield return session;
+            }
+        }
+
+        public async Task<Session?> GetByIdAsync(int id)
+            => await _context.Sessions.FindAsync(id);
+
+        public async Task CreateAsync(Session session)
+        {
+            if (!await _context.Randonnees.AnyAsync(r => r.Id == session.RandonneeId) && !await _context.Themes.AnyAsync(t => t.Id == session.ThemeId))
+            {
+                throw new InvalidOperationException("La randonnée ou le thème spécifié n'existe pas.");
+            }
+            await _context.Sessions.AddAsync(session);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Session session)
+        {
+            _context.Update(session);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var session = await _context.Sessions.FindAsync(id);
+            if (session != null)
+            {
+                _context.Sessions.Remove(session);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
