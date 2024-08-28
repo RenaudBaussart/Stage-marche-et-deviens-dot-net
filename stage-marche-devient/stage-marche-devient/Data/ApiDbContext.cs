71a615c3dc2b531ca3c16eed81c11a8971c0a116
Lucas Kucheida <kucheida.lucas@gmail.com>
2024-08-28 11:30:16 +0200

    feat(ApiDbContext.cs): Creation du DbContext

---
+++ stage-marche-devient/stage-marche-devient/Data/ApiDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using stage_marche_devient.Models;
+
+namespace stage_marche_devient.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options) { }
+
+        public DbSet<Session> Sessions { get; set; }
+
+    }
+}
